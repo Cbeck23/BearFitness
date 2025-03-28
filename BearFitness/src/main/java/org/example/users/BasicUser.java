f29a662cb331a47aa428e723b1dec4d782d70e57
Cbeck23 <caleb_beckering1@baylor.edu>
2025-03-28 15:09:01 -0500

    Starting adding BasicUser exercise planning functionality

--- BearFitness/src/main/java/org/example/users/BasicUser.java
+++ BearFitness/src/main/java/org/example/users/BasicUser.java
@@ -1,10 +1,31 @@
 package org.example.users;
 
+import java.util.List;
+
 public class BasicUser extends User {
+
+    List<ExercisePlan> plans;
+
     public BasicUser(String username, String password, String email) {
         super(username, password, email);
     }
 
+    public List<ExercisePlan> getPlans() {
+        return plans;
+    }
+
+    public void addPlan(ExercisePlan plan) {
+        plans.add(plan);
+    }
+
+    public void createPlan(){
+        boolean done = false;
+
+        while(!done){
+            //add workout entrys to plan, save plans
+
+        }
+    }
 
 
 }
